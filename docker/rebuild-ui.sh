00b047958620ad3355d99410d645efe9a21fed0d
Kevin Chen <kevinmichaelchen@gmail.com>
2017-12-04 11:56:52 -0500

    Write to dist-development-container

--- docker/rebuild-ui.sh
+++ docker/rebuild-ui.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_DIR/get-environment.sh
 
 printf "$yellow" "❤ Building Docker image for $ui_project_name..."
-rm -rf $ui_docker_context/dist-production
+rm -rf $ui_docker_context/dist-development-container
 npm --prefix $ui_docker_context run build-development-container
 docker build -t teslagov/$ui_project_name:$version $ui_docker_context --no-cache
