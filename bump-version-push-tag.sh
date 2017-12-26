0dc64cbd0efddb8682d3378c5b225953793d1d44
Kevin Chen <kevinmichaelchen@gmail.com>
2017-12-26 15:44:40 -0500

    bash functions

--- bump-version-push-tag.sh
+++ bump-version-push-tag.sh
@@ -6,7 +6,7 @@ source $SCRIPT_DIR/colors.sh
 
 function usage() {
     echo ""
-    echo "usage: ./bump-version-push-tag.sh [major|minor|patch]"
+    echo "usage: ./bump-version-push-tag.sh [major|minor|patch] [pathToGitRepo]"
     echo ""
     echo "For instance,"
     echo ""
@@ -15,12 +15,49 @@ function usage() {
     echo "(Case matters. 'major' is a valid argument. 'MAJOR' is not.)"
 }
 
-if [ $# -lt 1 ]; then
+function ensure_repo_not_dirty() {
+    local repo_path
+    repo_path=$1
+    git_description="$(git -C $repo_path describe --always --tags --dirty)"
+    if [[ $git_description =~ ^.*-dirty$ ]] ;
+    then
+        echo Repo is dirty. Please commit first.
+        exit 1
+    fi
+}
+
+function fetch_tags() {
+    local repo_path
+    repo_path=$1
+    git -C $repo_path fetch --tags
+}
+
+function get_latest_tag() {
+    local repo_path default_tag git_tag
+
+    repo_path=$1
+    default_tag=$2
+    git_tag=$(git -C ${repo_path} tag --sort=v:refname | tail -n 1)
+
+    if [ -z "$default_tag" ]; then
+        default_tag=v0.0.1
+    fi
+
+    if [ -z "$git_tag" ]; then
+        git_tag=$default_tag
+    fi
+
+    echo -n $git_tag
+}
+
+#function get_incremented_tag() {
+#}
+
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then
     usage
     exit 1
 fi
 
-# Ensure the selected environment is valid.
 case "$1" in
     major) semver_flag=-M
            ;;
@@ -31,30 +68,22 @@ case "$1" in
     *) usage; exit 1;;
 esac
 
-GIT_DESC="$(git describe --always --tags --dirty)"
-echo "Git describe returns $GIT_DESC"
-if [[ $GIT_DESC =~ ^.*-dirty$ ]] ;
-then
-	echo Repo is dirty. Please commit first.
-	exit 1
-fi
-
-git fetch --tags
-
-LATEST_TAG=$(git tag --sort=v:refname | tail -n 1)
-
-if [ -z "$LATEST_TAG" ]
-then
-    echo "No tag found... Setting default tag of v0.0.1"
-    NEW_TAG=v0.0.1
+if [ -z "$2" ]; then
+    repo_path=$(realpath .)
 else
-    echo "Old tag was $LATEST_TAG"
-    NEW_TAG=$($SCRIPT_DIR/semver/increment_version.sh $semver_flag $LATEST_TAG)
+    repo_path=$(realpath "$2")
 fi
 
-echo "New tag is $NEW_TAG"
+ensureRepoNotDirty "$repo_path"
+fetch_tags "$repo_path"
 
-#git tag -a $NEW_TAG -m "$NEW_TAG"
-#git push origin $NEW_TAG
+current_tag=$(get_latest_tag "$repo_path" "v0.0.1")
+echo "Latest tag is $current_tag"
+
+new_tag=$($SCRIPT_DIR/semver/increment_version.sh $semver_flag $current_tag)
+echo "New tag is $new_tag"
+
+git -C $repo_path tag -a $new_tag -m "$new_tag"
+git -C $repo_path push origin $new_tag
 
 echo "Done"
